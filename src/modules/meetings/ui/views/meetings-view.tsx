f3f5816d8e6ee48a401d8be866d7c9ebc0cf44ba
BoduBhai <bodruddozaaraf@gmail.com>
2025-06-13 08:16:24 +0600

    Meetings Data Table

--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -3,6 +3,9 @@
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
+import { DataTable } from "@/components/data-table";
+import { columns } from "../components/columns";
+import { EmptyState } from "@/components/empty-state";
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 
@@ -10,7 +13,17 @@ export const MeetingsView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
 
-  return <div className="overflow-x-scroll">{JSON.stringify(data)}</div>;
+  return (
+    <div className="flex flex-1 flex-col gap-y-4 px-4 pb-4">
+      <DataTable data={data.items} columns={columns} />
+      {data.items.length === 0 && (
+        <EmptyState
+          title="Create your first meeting"
+          description="Schedule a meeting to connext with others. Each meeting let's you collaborate, share ideas, and interact with participants in real-time."
+        />
+      )}
+    </div>
+  );
 };
 
 export const MeetingsLoadingState = () => {
